01f34feb9544d1f9edd1e1c0bac83b30464b85c1
isaias005 <isaias005@hotmail.com>
2020-07-28 19:31:33 -0400

    Initial commit

---
+++ config/plugins.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  email: {
+    provider: "nodemailer",
+    providerOptions: {
+      host: env("SMTP_HOST", "smtp.gmail.com"),
+      port: env("SMTP_PORT", 465),
+      auth: {
+        user: env("SMTP_USERNAME", "isaias0005@gmail.com"),
+        pass: env("SMTP_PASSWORD", "imtheboneofmysword0511"),
+      },
+    },
+    settings: {
+      defaultFrom: "isaias0005@gmail.com",
+      defaultReplyTo: "isaias0005@gmail.com",
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a39c3363-19a9-4ecc-8c60-2ae6e8703b6d'
+};
